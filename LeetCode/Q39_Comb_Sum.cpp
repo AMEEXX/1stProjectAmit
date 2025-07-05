5f8b1e908be8a7b42c04bc9bdaf13f8bfee9d99e
Amit Kumar Hota <b123013@iiit-bh.ac.in>
2025-07-05 21:42:22 +0000

    Q39 Comb Sum

---
+++ LeetCode/Q39_Comb_Sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void backtrack(vector<int>&x, int  i, int sum, vector<int>& candidates,vector<vector<int>>&result,int target){
+        if (i >= candidates.size()) return;
+
+        if(sum > target ) return;
+        if(sum == target) {
+            result.push_back(x);
+            return;
+        }
+        
+            x.push_back(candidates[i]);
+            backtrack(x,i,sum+candidates[i],candidates,result,target);
+        x.pop_back();
+            
+            backtrack(x,i+1,sum,candidates,result,target);
+        
+
+
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>result;
+        vector<int>x;
+        backtrack(x,0,0,candidates,result,target);
+        return result;
+        
+    }
+};
